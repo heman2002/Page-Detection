d133f1875e87fdbbf4dfcdb92d2723b3d5beee40
heman2002 <himanshu.singhal@rocketmail.com>
2017-07-18 18:55:39 +0530

    Create page.py

---
+++ page.py
@@ -0,0 +1,37 @@
+xrange = range
+
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+
+def angle_cos(p0, p1, p2):
+    d1, d2 = (p0-p1).astype('float'), (p2-p1).astype('float')
+    return abs( np.dot(d1, d2) / np.sqrt( np.dot(d1, d1)*np.dot(d2, d2) ) )
+
+def find_squares(img):
+    img = cv2.GaussianBlur(img, (5, 5), 0)
+    squares = []
+    for gray in cv2.split(img):
+        for thrs in xrange(0, 255, 26):
+            if thrs == 0:
+                bin = cv2.Canny(gray, 0, 50, apertureSize=5)
+                bin = cv2.dilate(bin, None)
+            else:
+                retval, bin = cv2.threshold(gray, thrs, 255, cv2.THRESH_BINARY)
+            bin, contours, hierarchy = cv2.findContours(bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            for cnt in contours:
+                cnt_len = cv2.arcLength(cnt, True)
+                cnt = cv2.approxPolyDP(cnt, 0.02*cnt_len, True)
+                x,y,w,h = cv2.boundingRect(cnt)
+                if len(cnt) == 4 and cv2.contourArea(cnt) > 5000 and cv2.isContourConvex(cnt):
+                    cnt = cnt.reshape(-1, 2)
+                    max_cos = np.max([angle_cos( cnt[i], cnt[(i+1) % 4], cnt[(i+2) % 4] ) for i in xrange(4)])
+                    if max_cos < 0.1 and w>1000 and h>1000:
+                        new_img=img[y:y+h,x:x+w]
+                        cv2.imwrite('output' + '.png', new_img)
+
+if __name__ == '__main__':
+ img = cv2.imread('hello.jpg')
+ find_squares(img)
+ 
